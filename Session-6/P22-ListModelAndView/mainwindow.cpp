35681b1a6ec8ef75ba803d9bc07661027c1f580d
Leo Chen <maverickcc@gmail.com>
2018-12-17 12:30:04 +0800

    Add p21, p22

---
+++ Session-6/P22-ListModelAndView/mainwindow.cpp
@@ -0,0 +1,53 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QStringListModel>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->initDb();
+    QSqlTableModel *model = initModel();
+    ui->tableView->setModel(model);
+    //ui->listView->setModel(this->initStringModel());
+    ui->listView->setModel(model);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::initDb()
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(":memory:");
+    db.open();
+    db.exec("CREATE TABLE price (name Text, price Currency)");
+    db.exec("INSERT INTO price (name, price) VALUES('Apple', 2.01)");
+    db.exec("INSERT INTO price (name, price) VALUES('Banana', 1.332)");
+    db.exec("INSERT INTO price (name, price) VALUES('Orange', 0.60)");
+    db.exec("INSERT INTO price (name, price) VALUES('Pear', 1.93)");
+}
+
+QSqlTableModel *MainWindow::initModel()
+{
+    QSqlTableModel *model = new QSqlTableModel(this);
+    model->setTable("price");
+    model->select();
+
+    model->setHeaderData(0, Qt::Horizontal, "Item Name");
+    model->setHeaderData(1, Qt::Horizontal, "Price");
+    return model;
+}
+
+QAbstractItemModel *MainWindow::initStringModel()
+{
+    QStringList list({"Parsley", "Sage", "Rosemary", "Thyme"});
+    QStringListModel *model = new QStringListModel(list, this);
+    model->setData(model->index(3), "Cinnamon");
+    return model;
+}
---
+++ Session-6/P22-ListModelAndView/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSqlTableModel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    void initDb();
+    QSqlTableModel *initModel();
+    QAbstractItemModel *initStringModel();
+
+};
+
+#endif // MAINWINDOW_H
